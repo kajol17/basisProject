f36107d8c8bcd6ef64e4951e10c64b0268e01ba4
kajol17 <kajol@valuefy.com>
2020-04-12 16:28:07 +0530

    created comment validation flow

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const commentModel = require("../models/comment.js");
+const validationUtils = require("../validation-utils.js");
 
 router.post("/create", async (req, res) => {
+  validationUtils.validateRequest(req, res);
+  validationUtils.validateWritePermission(req, res);
+
   const comment = new commentModel({
     postId: req.body.postId,
     uploaderId: req.body.uploaderId,

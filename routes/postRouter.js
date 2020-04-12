9a1d0353685a3d891fb639ae5ebc67747aa196d0
kajol17 <kajol@valuefy.com>
2020-04-12 16:20:22 +0530

    moved validation in seperate file

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const postModel = require("../models/post.js");
-const jwt = require("jsonwebtoken");
+const validationUtils = require("../validation-utils.js");
 
 router.post("/create", async (req, res) => {
-  validateRequest(req, res);
-  validateWritePermission(req, res);
+  validationUtils.validateRequest(req, res);
+  validationUtils.validateWritePermission(req, res);
 
   const post = new postModel({
     content: req.body.content,
@@ -21,50 +21,6 @@ router.post("/create", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
-// Expected format of token
-//Authorization: Bearer <jwt_token>
-function appendToken(req, res) {
-  const header = req.get("Authorization");
-  if (typeof header !== "undefined") {
-    const headerSplit = header.split(" ");
-    const token = headerSplit[1];
-    req.token = token;
-  } else {
-    return res.status(500).json({ message: "User not logged in! " });
-  }
-}
-
-function verifyToken(req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err, data) => {
-    if (err) {
-      return res.status(403).json({ message: "User session expired!" });
-    } else {
-      req.user = data.user;
-    }
-  });
-}
-
-function validateRequest(req, res) {
-  appendToken(req, res);
-  verifyToken(req, res);
-}
-
-function validateWritePermission(req, res) {
-  var phoneNumber = req.user.phoneNumber;
-  if (isOdd(phoneNumber)) {
-    return res
-      .status(403)
-      .json({ message: "User Does not have write permission!" });
-  }
-}
-
-function isEven(number) {
-  return number % 2 == 0;
-}
-
-function isOdd(number) {
-  return !isEven(number);
-}
 
 router.get("/fetch", (req, res) => {
   var postIds = req.body.postIds;
---
+++ validation-utils.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+module.exports = {
+  validateRequest: function (req, res) {
+    appendToken(req, res);
+    verifyToken(req, res);
+  },
+
+  validateWritePermission: function (req, res) {
+    var phoneNumber = req.user.phoneNumber;
+    if (isOdd(phoneNumber)) {
+      return res
+        .status(403)
+        .json({ message: "User Does not have write permission!" });
+    }
+  },
+};
+
+// Expected format of token
+//Authorization: Bearer <jwt_token>
+function appendToken(req, res) {
+  const header = req.get("Authorization");
+  if (typeof header !== "undefined") {
+    const headerSplit = header.split(" ");
+    const token = headerSplit[1];
+    req.token = token;
+  } else {
+    return res.status(500).json({ message: "User not logged in! " });
+  }
+}
+
+function verifyToken(req, res) {
+  jwt.verify(req.token, process.env.SECRET_KEY, (err, data) => {
+    if (err) {
+      return res.status(403).json({ message: "User session expired!" });
+    } else {
+      req.user = data.user;
+    }
+  });
+}
+
+function isEven(number) {
+  return number % 2 == 0;
+}
+
+function isOdd(number) {
+  return !isEven(number);
+}

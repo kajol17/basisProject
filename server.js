c398ea67ba262aaf7fc1e3a052759945d02da14d
kajol17 <kajol@valuefy.com>
2020-04-12 23:09:43 +0530

    updated db connection properties

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const postRouter = require("./routes/postRouter.js");
 const commentRouter = require("./routes/commentRouter.js");
 
 require("dotenv").config();
+mongodb_connection_string = process.env.DATABASE_URL;
+mongoose.connect(mongodb_connection_string, { useNewUrlParser: true });
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
@@ -15,4 +16,6 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/post", postRouter);
 app.use("/comment", commentRouter);
-app.listen(3000, () => console.log("Server started"));
+
+var server_port = process.env.SERVER_PORT;
+app.listen(server_port, () => console.log("Server started"));
